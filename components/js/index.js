b7cd58b1f2f59b9d8dfc96418a7fb586888aaa75
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2017-09-26 14:25:07 +0800

    Adding

--- components/js/index.js
+++ components/js/index.js
@@ -1,4 +1,30 @@
-new Vue({
-    el: '#app'
+// The component extends the Vue instance
+Vue.component('my-cmp', {
+    // declare as function so it does not interfere
+    // with the Vue instance
+    data: function () {
+        return {
+            status: 'Critical'
+        }
+    },
+    template: '<p>Server Status: {{ status }}</p>'
+});
 
+// Declare component as variable
+var person = ('my-cmp', {
+    // declare as function so it does not interfere
+    // with the Vue instance
+    data: function () {
+        return {
+            name: 'Prajesh'
+        }
+    },
+    template: '<p>Hello: {{ name }}</p>'
+});
+
+new Vue({
+    el: '.app',
+    components: {
+        'person-cmp' : person
+    }
 });
