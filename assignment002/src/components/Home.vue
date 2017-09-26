57efe6964433a5ea502449920896db02aa5972e8
Prajesh Ananthan <prajesh.ananthan@airasiabig.com>
2017-09-26 13:21:29 +0800

    Component practice assignment

---
+++ assignment002/src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="container">
+            <app-header></app-header>
+
+            <app-server></app-server>
+
+            <app-footer></app-footer>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue'
+    import Servers from './Servers.vue'
+    import Footer from './Footer.vue'
+
+    export default {
+        components: {
+            'app-header': Header,
+            'app-server': Servers,
+            'app-footer': Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
