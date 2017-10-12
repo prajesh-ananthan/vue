df1cfe333dba2abd21f69d447f598d9cf5af25ab
Prajesh Ananthan <prajesh.ananthan@outlook.com>
2017-10-12 10:10:29 +0800

    Default slots and slots default - Udemy

---
+++ advanced-components/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+            	<appQuote>
+            		<h2 slot="title">{{ quoteTitle }}</h2>
+            		<p slot="content">Wonderful Quote</p>
+            	</appQuote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+	import Quote from './components/Quote.vue'
+
+    export default {
+    	data: function() {
+    		return {
+    			quoteTitle: 'The Quote'
+    		}
+    	},
+
+    	components: {
+    		appQuote : Quote
+    	}
+        
+    }
+</script>
+
+<style>
+</style>
